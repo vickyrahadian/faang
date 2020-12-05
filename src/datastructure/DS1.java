3c035c628231e6f002d23257006282074a2460dd
Vicky Rahadian Firmansyah <vicky.rahadian@gmail.com>
2020-12-05 22:49:50 +0700

    tes

---
+++ src/datastructure/DS1.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+public class DS1 {
+    public static void main(String[] args) {
+
+        // [1, 2, 3, 4, 5]
+
+        int[] angka = {15, 22, 13, 14, 25};
+
+        System.out.println(angka[0]);
+
+    }
+}
